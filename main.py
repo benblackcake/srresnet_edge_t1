2dce6a691a46b6e33a4f61a3629c5e836a9d9c82
ben <pingheng.chang@gmail.com>
2020-06-03 20:16:12 +0800

    add log record function

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from benchmark import Benchmark
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--load', type=str, help='Checkpoint to load all weights from.')
-	parser.add_argument('--log-path', type=str, default='results/' help='Checkpoint to load all weights from.')
+	parser.add_argument('--log-path', type=str, default='results/', help='Checkpoint to load all weights from.')
 	parser.add_argument('--batch-size', type=int, default=128, help='Mini-batch size.')
 	parser.add_argument('--learning-rate', type=float, default=1e-4, help='Learning rate for Adam.')
 	parser.add_argument('--train-dir', type=str, help='Directory containing training images')
